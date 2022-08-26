82ca2b547cb920deaedbec6cfedd66c38c136432
Neroslike <obduvi22@hotmail.com>
2022-08-26 14:16:59 -0400

    Implement user Log in Add self association for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  belongs_to :user
-  has_many :friends, class_name: :user, foreign_key: :user_id
+  belongs_to :user_friend, class_name: 'User', foreign_key: 'user_id', optional: true
+  has_many :friends, class_name: 'User', foreign_key: 'user_id'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
