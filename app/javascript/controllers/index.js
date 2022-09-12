1323a65fe0e2e7375af6cbb5b295b7592464c363
Neroslike <obduvi22@hotmail.com>
2022-09-12 16:14:46 -0400

    Add new comment box (not functional)

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,10 @@
 // Import and register all your controllers from the importmap under controllers/*
 
 import { application } from "controllers/application"
+import TextareaAutogrow from 'stimulus-textarea-autogrow'
+
+// Stimulus textarea autogrow component
+application.register('textarea-autogrow', TextareaAutogrow)
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
