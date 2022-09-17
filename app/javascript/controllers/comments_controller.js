ea58291b385e491873d5cff296ca4b23b112b804
Neroslike <obduvi22@hotmail.com>
2022-09-17 11:42:02 -0400

    Add a new comment input Show comments in index page Persist changes upon clicking the back button when showing a post

---
+++ app/javascript/controllers/comments_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="comments"
+export default class extends Controller {
+  static targets = ['textArea']
+
+  // copyAndSubmit() {
+  //   this.textInputTarget.value = this.editableDivTarget.innerHTML
+  //   this.element.submit();
+  //   this.editableDivTarget.innerHTML = ''
+  // }
+
+  connect() {
+    // const tx = document.getElementsByTagName("textarea")
+    const tx = this.textAreaTargets
+    for (let i = 0; i < tx.length; i++) {
+      tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+      tx[i].addEventListener("input", OnInput, false);
+    }
+
+    function OnInput() {
+      this.style.height = 0;
+      this.style.height = (this.scrollHeight) + "px";
+    }
+  }
+
+  submit(e) {
+    if(e.keyCode == 13 && !e.shiftKey) {
+      this.submitAndCleanup(e)
+    }
+  }
+
+  submitAndCleanup(e) {
+    e.preventDefault()
+    this.element.submit();
+    this.textAreaTarget.value = ''
+  }
+}
+
